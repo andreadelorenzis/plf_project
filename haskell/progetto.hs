c1f52e2ca72e68e50d5e71bc0468381bf360aa6f
andrea.delorenzis <andrea.delorenzis@aizoongroup.com>
2023-08-03 22:37:57 +0200

    Finish code

--- haskell/progetto.hs
+++ haskell/progetto.hs
@@ -1,37 +1,92 @@
-{- Importazione delle librerie -}
-import Text.Printf (printf)
-import Data.List (sortOn)
-import Data.Function (on)
-import Text.Read (reads)
-import Data.List (nub)
-import Text.Read (readMaybe)
+{- IMPORTAZIONE DELLE LIBRERIE -}
+
+{- Formattazione e output -}
+import Text.Printf (printf)  -- Utilizzato per formattare le stringhe di output
+{- Gestione delle liste -}
+import Data.List (sortOn, nub)  -- Utilizzato per ordinare liste e rimuovere duplicati
+import Data.Function (on)  -- Utilizzato per personalizzare l'ordinamento in base a una funzione
+{- Conversione e lettura -}
+import Text.Read (reads, readMaybe)  -- Utilizzato per leggere input in modo sicuro e per effettuare parsing
+{- Controllo dei caratteri -}
+import Data.Char (isPrint)           
+
+{- MAIN -}
+
+{- Punto iniziale del programma -}
+main :: IO ()
+main = do
+  putStrLn "\n"
+  printHorizontalLine
+  putStrLn "Progetto del corso di Programmazione Logica e Funzionale"
+  putStrLn "Anno 2022/2023"
+  putStrLn "Progetto realizzato da: Andrea De Lorenzis\n"
+  mainMenu
+  printHorizontalLine
+
+{- Funzione Main contenente il menu di selezione dell'operazione da svolgere -}
+mainMenu :: IO ()
+mainMenu = do
+  putStrLn "\nSelezionare l'operazione da svolgere:"
+  putStrLn "1 - Linear Regression"
+  putStrLn "2 - K-Nearest Neighbors (KNN)"
+  putStrLn "3 - Esci"
+
+  choice <- getLine
+  case choice of
+    "1" -> do
+      putStrLn ""
+      putStrLn "-------- Linear Regression --------"
+      points <- readDataset
+      putStrLn "\nAdattamento della retta al dataset.."
+      let (slope, intercept) = linearRegression points
+      let formattedSlope = printf "%.2f" slope
+      let formattedIntercept = printf "%.2f" intercept
+      putStrLn $ "\nRetta interpolatrice: y = " ++ formattedSlope ++ "x + " ++ formattedIntercept
+      evaluateXValuesLoop (slope, intercept)
+      mainMenu
+
+    "2" -> do
+      putStrLn ""
+      putStrLn "-------- K-Nearest Neighbors --------"
+      dataset <- readLabeledDataset
+      let numPoints = length dataset
+      k <- readKValue numPoints
+      evaluatePointsLoop k dataset
+      mainMenu
+      
+    "3" -> putStrLn "\nChiusura del programma."
+    _ -> do
+      putStrLn "\nScelta incorretta. Seleziona un'opzione valida."
+      mainMenu
+
+{- DEFINIZIONE DI STRUTTURE DATI -}
 
 {- Definizione di un tipo per rappresentare punti in uno spazio 2D -}
 data Point2D = Point2D { xCoord :: Double, yCoord :: Double } deriving Show
 
 {- Definizione di un tipo per rappresentare i punti 2D etichettati con una classe (di tipo Int) -}
-data PointLabeled = PointLabeled { x :: Double, y :: Double, label :: Char }
+data LabeledPoint2D = LabeledPoint2D { x :: Double, y :: Double, label :: String } deriving Show
 
 {- CALCOLO DELLA REGRESSIONE LINEARE -}
 
-{- Calcola la media di una lista di Double -}
+{- Funzione che calcola la media di una lista di Double -}
 mean :: [Double] -> Double
 mean xs = sum xs / fromIntegral (length xs)
 
-{- Calcola la varianza di una lista di Double -}
+{- Funzione che calcola la varianza di una lista di Double -}
 variance :: [Double] -> Double
 variance xs = sum [(x - m) ** 2 | x <- xs] / fromIntegral (length xs)
   where
     m = mean xs
 
-{- Calcola la covarianza di due liste di Double -}
+{- Funzione che calcola la covarianza di due liste di Double -}
 covariance :: [Double] -> [Double] -> Double
 covariance xs ys = sum [(x - mx) * (y - my) | (x, y) <- zip xs ys] / fromIntegral (length xs)
   where
     mx = mean xs
     my = mean ys
 
-{- Calcola i coefficienti (pendenza e intercetta) della retta interpolatrice -}
+{- Funzione che calcola i coefficienti (pendenza e intercetta) della retta interpolatrice -}
 linearRegression :: [Point2D] -> (Double, Double)
 linearRegression points = (slope, intercept)
   where
@@ -40,26 +95,40 @@ linearRegression points = (slope, intercept)
     slope = covariance xs ys / variance xs
     intercept = mean ys - slope * mean xs
 
+{- Funzione che converte una coppia di Double in una struttura Point2D -}
 tupleToPoint2D :: [(Double, Double)] -> [Point2D]
 tupleToPoint2D = map (\(x, y) -> Point2D x y)
 
 {- Funzione per ottenere dall'utente una lista di punti separati da spazio -}
-readPoints :: IO [Point2D]
-readPoints = do
+readDataset :: IO [Point2D]
+readDataset = do
     putStrLn "\nInserisci i punti del dataset nel formato [(x1,y1), ..., (xn,yn)]:"
     input <- getLine
-    let parsedList = read input :: [(Double, Double)]
-    if length parsedList < 2
-        then do
-            putStrLn "Inserisci almeno due punti."
-            readPoints
-        else return $ tupleToPoint2D parsedList
+    let parsedList = readMaybe input :: Maybe [(Double, Double)]
+    case parsedList of
+        Nothing -> do
+            putStrLn "\nFormato non valido. Inserisci i punti nel formato [(x1, y1), ..., (xn, yn)]."
+            readDataset
+        Just points -> if length points < 2
+                       then do
+                           putStrLn "\nInput invalido. Inserisci almeno due punti."
+                           readDataset
+                       else return $ tupleToPoint2D points
 
-{- Funzione che converte una lista di Double in una lista di Point2D -}
-makePoints :: [Double] -> [Point2D]
-makePoints [] = []
-makePoints (x:y:rest) = Point2D x y : makePoints rest
-makePoints _ = error "Numero incorretto di coordinate."
+{- Funzione che cicla continuamente per valutare nuovi valori di x -}
+evaluateXValuesLoop :: (Double, Double) -> IO ()
+evaluateXValuesLoop (slope, intercept) = do
+  xValue <- readXValue
+  case xValue of
+    Just x -> do
+      let prediction = evaluateXValue x (slope, intercept)
+      let formattedPrediction = printf "%.2f" prediction
+      putStrLn $ "\nPer X = " ++ show x ++ " il valore previsto e' Y = " ++ formattedPrediction
+      continue <- askContinue
+      if continue
+        then evaluateXValuesLoop (slope, intercept)
+        else return ()
+    Nothing -> return ()  -- Ritorna al menu
 
 {- Funzione che ottiene dall'utente un punto da valutare sulla retta ottenuta -}
 readXValue :: IO (Maybe Double)
@@ -79,72 +148,102 @@ evaluateXValue x (slope, intercept) = slope * x + intercept
 {- CALCOLO DEL KNN -}
 
 {- Funzione che calcola la distanza euclidea tra due punti -}
-distance :: PointLabeled -> PointLabeled -> Double
+distance :: LabeledPoint2D -> LabeledPoint2D -> Double
 distance p1 p2 = sqrt $ (x p1 - x p2) ^ 2 + (y p1 - y p2) ^ 2
 
 {- Funziona che trova i k vicini per un punto dal dataset -}
-kNearestNeighbors :: Int -> PointLabeled -> [PointLabeled] -> [PointLabeled]
+kNearestNeighbors :: Int -> LabeledPoint2D -> [LabeledPoint2D] -> [LabeledPoint2D]
 kNearestNeighbors k testPoint dataset =
   take k $ sortOn (distance testPoint) dataset
 
 {- Funzione che trova la classe di maggioranza tra i vicini -}
-
-majorityClass :: [PointLabeled] -> Char
+majorityClass :: [LabeledPoint2D] -> String
 majorityClass neighbors =
   let allLabels = nub $ map label neighbors  -- Extract all unique labels from the neighbors
       counts = map (\label' -> (label', length (filter (\p -> label p == label') neighbors))) allLabels
       sortedCounts = reverse $ sortOn snd counts
   in fst $ head sortedCounts
 
-
-tupleToPointLabeled :: [(Double, Double, Char)] -> [PointLabeled]
-tupleToPointLabeled = map (\(x, y, l) -> PointLabeled x y l)
-
 {- Funzione per ottenere dall'utente un dataset di punti etichettati con una classe -}
-readLabeledDataset :: IO [PointLabeled]
+readLabeledDataset :: IO [LabeledPoint2D]
 readLabeledDataset = do
-    putStrLn "\nInserisci i punti etichettati del dataset nel formato [(x1,y1,'<Char>'), ..., (xn,yn,'<Char>')]:"
+    putStrLn "\nInserisci i punti etichettati del dataset nel formato [(x1,y1,'<class>'), ..., (xn,yn,'<class>')]:"
     input <- getLine
-    let parsedList = read input :: [(Double, Double, Char)]
-    if length parsedList < 2
-        then do
-            putStrLn "\nErrore. Inserisci almeno due punti.\n"
+    case reads input of
+        [(points, "")] ->
+            if all isValidLabeledPoint points && length points >= 2
+            then return $ tupleToLabeledPoint2D points
+            else do
+                putStrLn $ "\nErrore. " ++ errorMsg points
+                readLabeledDataset
+        _ -> do
+            putStrLn "\nErrore. Assicurati che l'input sia nel formato [(x1,y1,'<class>'), ..., (xn,yn,'<class>')]."
             readLabeledDataset
-        else return $ tupleToPointLabeled parsedList
+    where
+        errorMsg ps
+            | length ps < 2 = "Inserisci almeno due punti."
+            | not (all isValidLabeledPoint ps) = "Formato non valido. Controlla che ogni punto sia nel formato (x, y, '<class>') dove '<class>' è un carattere o una combinazione di caratteri."
+            | otherwise = ""
 
-{- Funzione per ottenere dall'utente un punto da testare sul KNN -}
-readPoint :: IO PointLabeled
-readPoint = do
-  putStrLn "\nInserisci il punto da valutare nel formato 'x y':"
-  input <- getLine
-  let [x, y] = map read $ words input
-  return $ PointLabeled x y 'Z'  -- The class label for the test point is not relevant, so we set it to Z.
+{- Converte una tripla (Double, Double, C) in una struttura LabeledPoint2D -}
+tupleToLabeledPoint2D :: [(Double, Double, String)] -> [LabeledPoint2D]
+tupleToLabeledPoint2D = map (\(x, y, l) -> LabeledPoint2D x y l)
 
-evaluateXValuesLoop :: (Double, Double) -> IO ()
-evaluateXValuesLoop (slope, intercept) = do
-  xValue <- readXValue
-  case xValue of
-    Just x -> do
-      let prediction = evaluateXValue x (slope, intercept)
-      let formattedPrediction = printf "%.2f" prediction
-      putStrLn $ "\nPer X = " ++ show x ++ " il valore previsto e' Y = " ++ formattedPrediction
-      continue <- askContinue
-      if continue
-        then evaluateXValuesLoop (slope, intercept)
-        else return ()
-    Nothing -> return ()  -- Return to the main menu
+{- Funzione per assicurarsi che il carattere sia stampabile -}
+isValidLabeledPoint :: (Double, Double, String) -> Bool
+isValidLabeledPoint (_, _, c) = all isPrint c -- Ensures the character is printable (i.e., not a control character)
 
-evaluateKNNLoop :: Int -> [PointLabeled] -> IO ()
-evaluateKNNLoop k dataset = do
+{- Funzione per ottenere dall'utente il valore di k -}
+readKValue :: Int -> IO Int
+readKValue numPoints = do
+    putStrLn $ "\nInserisci il valore di k (numero dei vicini, 1-" ++ show numPoints ++ "):"
+    input <- getLine
+    case readMaybe input of
+        Just k
+            | k > 0 && k <= numPoints -> return k
+            | otherwise -> do
+                putStrLn $ "\nIl valore di k deve essere un intero positivo compreso tra 1 e " ++ show numPoints ++ "."
+                readKValue numPoints
+        Nothing -> do
+            putStrLn "\nInput non valido. Inserisci un numero intero valido."
+            readKValue numPoints
+
+{- Funzione che cicla continuamente per valutare nuovi punti -}
+evaluatePointsLoop :: Int -> [LabeledPoint2D] -> IO ()
+evaluatePointsLoop k dataset = do
   testPoint <- readPoint
   let kNearest = kNearestNeighbors k testPoint dataset
       testClass = majorityClass kNearest
+  printNeighbors kNearest
   putStrLn $ "\nClasse prevista per il punto: " ++ show testClass
   continue <- askContinue
   if continue
-    then evaluateKNNLoop k dataset
+    then evaluatePointsLoop k dataset
     else return ()
 
+{- Funzione per ottenere dall'utente un punto da testare sul KNN -}
+readPoint :: IO LabeledPoint2D
+readPoint = do
+  putStrLn "\nInserisci il punto da valutare nel formato '(x, y)':"
+  input <- getLine
+  case readMaybe input of
+    Just (x, y) -> return $ LabeledPoint2D x y "Z"  -- Qui l'etichetta della classe non è significativa
+    Nothing -> do
+      putStrLn "\nInput incorretto. Inserisci un punto nel formato '(x, y)'."
+      readPoint
+
+{- Funzione per stampare i vicini -}
+printNeighbors :: [LabeledPoint2D] -> IO ()
+printNeighbors neighbors = do
+    putStrLn "\nI vicini del punto sono:"
+    mapM_ printNeighbor neighbors
+  where
+    printNeighbor (LabeledPoint2D x y lbl) =
+      putStrLn $ "Punto: (" ++ show x ++ ", " ++ show y ++ ", " ++ lbl ++ ")"
+
+{- FUNZIONI AUSILIARIE -}
+
+{- Funzione ausiliaria per chiedere all'utente se vuole continuare -}
 askContinue :: IO Bool
 askContinue = do
   putStrLn "\nVuoi continuare? (s/n)"
@@ -156,55 +255,6 @@ askContinue = do
       putStrLn "\nInput invalido. Inserisci 's' per continuare o 'n' per ritornare al menu principale"
       askContinue
 
-{- Helper function to print a horizontal line -}
+{- Funzione ausiliaria per stampare una riga orizzontale -}
 printHorizontalLine :: IO ()
 printHorizontalLine = putStrLn "---------------------------------------------------------------"
-
-{- MAIN -}
-
-{- Funzione Main contenente il menu di selezione dell'operazione da svolgere -}
-mainMenu :: IO ()
-mainMenu = do
-  putStrLn "\nSelezionare l'operazione da svolgere:"
-  putStrLn "1 - Linear Regression"
-  putStrLn "2 - K-Nearest Neighbors (KNN)"
-  putStrLn "3 - Esci"
-
-  choice <- getLine
-  case choice of
-    "1" -> do
-      putStrLn ""
-      putStrLn "-------- Linear Regression --------"
-      points <- readPoints
-      putStrLn "\nAdattamento della retta al dataset.."
-      let (slope, intercept) = linearRegression points
-      let formattedSlope = printf "%.2f" slope
-      let formattedIntercept = printf "%.2f" intercept
-      putStrLn $ "\nRetta interpolatrice: y = " ++ formattedSlope ++ "x + " ++ formattedIntercept
-      evaluateXValuesLoop (slope, intercept)
-      mainMenu
-
-    "2" -> do
-      putStrLn ""
-      putStrLn "-------- K-Nearest Neighbors --------"
-      dataset <- readLabeledDataset
-      putStrLn "\nInserisci il valore di k (numero dei vicini):"
-      k <- readLn :: IO Int
-      evaluateKNNLoop k dataset
-      mainMenu
-      
-    "3" -> putStrLn "\nChiusura del programma."
-    _ -> do
-      putStrLn "Scelta incorretta. Seleziona un'opzione valida."
-      mainMenu
-
-{- Punto iniziale del programma -}
-main :: IO ()
-main = do
-  putStrLn "\n"
-  printHorizontalLine
-  putStrLn "Progetto del corso di Programmazione Logica e Funzionale"
-  putStrLn "Anno 2022/2023"
-  putStrLn "Progetto realizzato da: Andrea De Lorenzis\n"
-  mainMenu
-  printHorizontalLine
--- prolog/progetto.pl
+++ prolog/progetto.pl
@@ -23,89 +23,21 @@ main_menu :-
    Choice = 3 -> nl, write('Chiusura del programma.'), nl ;
    write('Scelta incorretta. Per favore, seleziona una delle opzioni valide.'), nl, 
    main_menu). 
-   
+
+% CALCOLO REGRESSIONE LINEARE
+
 /* Funzione per eseguire la regressione lineare */
 doLinearRegression :-
   nl, write('-------- Linear Regression --------'), nl,
-  readPoints(Points),
+  readDataset(Points),
   nl, write('Adattamento della retta al dataset..'), nl,
   linearRegression(Points, Slope, Intercept), 
   nl,
   format('Retta interpolatrice: y = ~2fx + ~2f~n', [Slope, Intercept]),
-  evaluateXValues(Slope, Intercept).
-  
-/* Funzione per eseguire il KNN */
-doKNN :- 
-    nl, write('-------- K-Nearest Neighbors --------'), nl,
-    readLabeledDataset(Dataset),
-	length(Dataset, NumPoints),
-    repeat,
-    nl, write('Inserisci il valore per k: '), nl,
-    read(K),
-    (   number(K) -> 
-        K > 0,
-		(K =< NumPoints ->
-            !
-		;
-		    nl, write('Input invalido. Inserisci un intero positivo '),
-			write('minore del (o uguale al) numero totale di punti nel dataset'), nl,
-			format('(Numero di punti: ~d)', [NumPoints]), nl,
-            fail
-		)
-    ;
-        nl, write('Input invalido. Inserisci un intero positivo.'), nl,
-        fail
-    ),
-    evaluatePoints(Dataset, K).
+  evaluateXValuesLoop(Slope, Intercept).
 
- 
-% CALCOLO REGRESSIONE LINEARE
-
-/* Calcola la media di una lista di numeri */
-mean([], 0).
-mean([X | Xs], Mean) :-
-    mean(Xs, RestMean),
-    length(Xs, N),
-    Mean is (X + N * RestMean) / (N + 1).
-  
-/* Calcola la varianza di una lista di numeri */  
-variance([], 0).
-variance([X | Xs], Var) :-
-    mean([X | Xs], Mean),
-    variance(Xs, RestVar),
-    Var is RestVar + (X - Mean) * (X - Mean).
-	
-/* Calcola la covarianza di due liste di numeri */
-covariance([], [], 0).
-covariance([X | Xs], [Y | Ys], Cov) :-
-  mean([X | Xs], MeanX),
-  mean([Y | Ys], MeanY),
-  covariance(Xs, Ys, RestCov),
-  Cov is RestCov + (X - MeanX) * (Y - MeanY).
-  
-/* Calcola i coefficienti (pendenza e intercetta) della retta interpolatrice */
-linearRegression(Points, Slope, Intercept) :-
-  extractXCoords(Points, Xs),
-  extractYCoords(Points, Ys),
-  variance(Xs, VarX),
-  covariance(Xs, Ys, CovXY),
-  Slope is CovXY / VarX,
-  mean(Ys, MeanY),
-  mean(Xs, MeanX),
-  Intercept is MeanY - Slope * MeanX.
- 
-/* Estra le coordinate X da una lista di punti */
-extractXCoords([], []).
-extractXCoords([(X, _) | Points], [X | Xs]) :-
-  extractXCoords(Points, Xs).
-  
-/* Estrae le coordinate Y da una lista di punti */
-extractYCoords([], []).
-extractYCoords([(_, Y) | Points], [Y | Ys]) :-
-  extractYCoords(Points, Ys).
-  
 /* Predicato per leggere una lista di tuple da tastiera */
-readPoints(List) :-
+readDataset(List) :-
     repeat,
     nl, write('Inserisci i punti del dataset nel formato: [(x1,y1), ..., (xn,yn)]:'), nl,
     read(List),
@@ -128,7 +60,59 @@ is_point(Point) :-
     Point = (X, Y),
     number(X),
     number(Y).
+
+/* Calcola i coefficienti (pendenza e intercetta) della retta interpolatrice */
+linearRegression(Points, Slope, Intercept) :-
+  extractXCoords(Points, Xs),
+  extractYCoords(Points, Ys),
+  variance(Xs, VarX),
+  covariance(Xs, Ys, CovXY),
+  Slope is CovXY / VarX,
+  mean(Ys, MeanY),
+  mean(Xs, MeanX),
+  Intercept is MeanY - Slope * MeanX.
+ 
+/* Estra le coordinate X da una lista di punti */
+extractXCoords([], []).
+extractXCoords([(X, _) | Points], [X | Xs]) :-
+  extractXCoords(Points, Xs).
   
+/* Estrae le coordinate Y da una lista di punti */
+extractYCoords([], []).
+extractYCoords([(_, Y) | Points], [Y | Ys]) :-
+  extractYCoords(Points, Ys).
+
+/* Calcola la varianza di una lista di numeri */  
+variance([], 0).
+variance([X | Xs], Var) :-
+    mean([X | Xs], Mean),
+    variance(Xs, RestVar),
+    Var is RestVar + (X - Mean) * (X - Mean).
+	
+/* Calcola la covarianza di due liste di numeri */
+covariance([], [], 0).
+covariance([X | Xs], [Y | Ys], Cov) :-
+  mean([X | Xs], MeanX),
+  mean([Y | Ys], MeanY),
+  covariance(Xs, Ys, RestCov),
+  Cov is RestCov + (X - MeanX) * (Y - MeanY).
+
+/* Calcola la media di una lista di numeri */
+mean([], 0).
+mean([X | Xs], Mean) :-
+    mean(Xs, RestMean),
+    length(Xs, N),
+    Mean is (X + N * RestMean) / (N + 1).
+
+/* Predicato che cicla continuamente per valutare i punti e chiede 
+   all'utente se vuole continuare */
+evaluateXValuesLoop(Slope, Intercept) :-
+    readXValue(Value),
+    evaluateXValue(Value, Slope, Intercept, Y),
+    nl, format('Per X = ~2f il valore previsto e\' Y = ~2f', [Value, Y]), nl,
+    askContinue(Continue),
+    (Continue = 's' -> evaluateXValuesLoop(Slope, Intercept) ; true).
+
 /* Funzione che ottiene dall'utente una coordinata X da valutare sulla retta ottenuta */
 readXValue(Value) :-
     repeat,
@@ -141,88 +125,35 @@ readXValue(Value) :-
         fail % Failure: Value is not a number, retry the input
     ).
 
-/* Predicato che cicla continuamente per valutare i punti e chiede 
-   all'utente se vuole continuare */
-evaluateXValues(Slope, Intercept) :-
-    readXValue(Value),
-    evaluateXValue(Value, Slope, Intercept, Y),
-    nl, format('Per X = ~2f il valore previsto e\' Y = ~2f', [Value, Y]), nl,
-    askContinue(Continue),
-    (Continue = 's' -> evaluateXValues(Slope, Intercept) ; true).
-
 /* Funzione che valuta la regressione lineare per il punto */
 evaluateXValue(X, Slope, Intercept, Y) :-
   Y is Slope * X + Intercept.
 
 % CALCOLO K-NEAREST NEIGHBORS
 
-/* Funzione che calcola la distanza euclidea tra due punti */
-distance((X1, Y1, _), (X2, Y2), Dist) :-
-  Dist is sqrt((X1 - X2) * (X1 - X2) + (Y1 - Y2) * (Y1 - Y2)).
-  
-/* Funzione che trova i k vicini per un punto */
-kNearestNeighbors(K, TestPoint, Dataset, Neighbors) :-
-  calculateDistances(TestPoint, Dataset, Distances),
-  sortDistances(Distances, SortedDistances),
-  take(K, SortedDistances, NeighborsPairs),
-  convertToNeighbors(NeighborsPairs, Neighbors).
-  
-/* Rimuove il valore della distanza dal formato (Dist-(X, Y, Class)) 
-   per ottenere il formato (X, Y, Class) */
-convertToNeighbors([], []).
-convertToNeighbors([_-Neighbor | RestPairs], [Neighbor | RestNeighbors]) :-
-    convertToNeighbors(RestPairs, RestNeighbors).
-
-/* Predicato per calolcare le distanze tra TestPoint e ogni altro punto del dataset */
-calculateDistances(_, [], []).
-calculateDistances(TestPoint, [Point | Rest], [Dist-Point | Distances]) :-
-  distance(Point, TestPoint, Dist),
-  calculateDistances(TestPoint, Rest, Distances).
-  
-/* Ordina le distanze in ordine crescente */
-sortDistances(Distances, SortedDistances) :-
-  keysort(Distances, SortedDistances).
-
-/* Prende i primi K elementi di una lista */
-take(0, _, []).
-take(K, [X | Xs], [X | Rest]) :-
-  K > 0,
-  K1 is K - 1,
-  take(K1, Xs, Rest).
-  
-/* Funzione che trova la classe di maggioranza tra i vicini */
-majorityClass(Neighbors, MajorityClass) :-
-    countClassOccurrences(Neighbors, CountMap),
-    maxClassOccurrences(CountMap, MajorityClass).
-
-/* Predicato ausiliario per conteggiare le occorrenze di ogni 
-  classe nei vicini */
-countClassOccurrences(Neighbors, CountMap) :-
-    countClassOccurrences(Neighbors, [], CountMap).
-
-countClassOccurrences([], CountMap, CountMap).
-countClassOccurrences([(X, Y, Class) | Neighbors], PartialCountMap, CountMap) :-
-    updateCountMap(Class, PartialCountMap, NewCountMap),
-    countClassOccurrences(Neighbors, NewCountMap, CountMap).
-
-updateCountMap(Class, [], [(Class, 1)]).
-updateCountMap(Class, [(Class, Count) | Rest], [(Class, NewCount) | Rest]) :-
-    NewCount is Count + 1.
-updateCountMap(Class, [(OtherClass, Count) | Rest], [(OtherClass, Count) | NewRest]) :-
-    Class \= OtherClass,
-    updateCountMap(Class, Rest, NewRest).
-
-/* Predicato ausiliario per trovare la classe con il maggior numero di 
-  occorrenze */
-maxClassOccurrences([(Class, Count) | Rest], MajorityClass) :-
-    maxClassOccurrences(Rest, Class, Count, MajorityClass).
-
-maxClassOccurrences([], MajorityClass, _, MajorityClass).
-maxClassOccurrences([(Class, Count) | Rest], MaxClass, MaxCount, MajorityClass) :-
-    ( Count > MaxCount ->
-        maxClassOccurrences(Rest, Class, Count, MajorityClass)
-    ;   maxClassOccurrences(Rest, MaxClass, MaxCount, MajorityClass)
-    ).
+/* Funzione per eseguire il KNN */
+doKNN :- 
+    nl, write('-------- K-Nearest Neighbors --------'), nl,
+    readLabeledDataset(Dataset),
+	length(Dataset, NumPoints),
+    repeat,
+    nl, write('Inserisci il valore per k: '), nl,
+    read(K),
+    (   number(K) -> 
+        K > 0,
+		(K =< NumPoints ->
+            !
+		;
+		    nl, write('Input invalido. Inserisci un intero positivo '),
+			write('minore del (o uguale al) numero totale di punti nel dataset'), nl,
+			format('(Numero di punti: ~d)', [NumPoints]), nl,
+            fail
+		)
+    ;
+        nl, write('Input invalido. Inserisci un intero positivo.'), nl,
+        fail
+    ),
+    evaluatePointsLoop(Dataset, K).
 
 /* Funzione per ottenere dall'utente un dataset di punti etichettati con 
    una classe */
@@ -252,6 +183,18 @@ is_labeled_point(LabeledPoint) :-
     number(Y),
     atom(C).
 
+/* Funzione per continuamente inserire punti da valutare con KNN */
+evaluatePointsLoop(_, 0) :- !.
+evaluatePointsLoop(Dataset, K) :-
+  readLabeledPoint(TestPoint),
+  kNearestNeighbors(K, TestPoint, Dataset, Neighbors),
+  nl, format('I ~d vicini del punto sono: ', [K]), nl, 
+  printNeighbors(Neighbors), nl, 
+  majorityClass(Neighbors, Class),
+  format('La classe prevista per il punto e\': ~w~n', [Class]), 
+  askContinue(Continue),
+  (Continue = 's' -> evaluatePointsLoop(Dataset, K) ; true).
+
 /* Funzione per leggere un singolo punto da valutare con KNN */
 readLabeledPoint(Point) :-
     repeat,
@@ -268,23 +211,79 @@ validPoint(_) :-
     nl, write('Punto non valido. Assicurati di inserire un punto nel formato (x,y).'), nl,
     fail.
 
-/* Funzione per continuamente inserire punti da valutare con KNN */
-evaluatePoints(_, 0) :- !.
-evaluatePoints(Dataset, K) :-
-  readLabeledPoint(TestPoint),
-  kNearestNeighbors(K, TestPoint, Dataset, Neighbors),
-  nl, format('I ~d vicini del punto sono: ', [K]), nl, 
-  printNeighbors(Neighbors), nl, 
-  majorityClass(Neighbors, Class),
-  format('La classe prevista per il punto e\': ~w~n', [Class]), 
-  askContinue(Continue),
-  (Continue = 's' -> evaluatePoints(Dataset, K) ; true).
+/* Funzione che trova i k vicini per un punto */
+kNearestNeighbors(K, TestPoint, Dataset, Neighbors) :-
+  calculateDistances(TestPoint, Dataset, Distances),
+  sortDistances(Distances, SortedDistances),
+  take(K, SortedDistances, NeighborsPairs),
+  convertToNeighbors(NeighborsPairs, Neighbors).
 
+/* Predicato per calolcare le distanze tra TestPoint e ogni altro punto del dataset */
+calculateDistances(_, [], []).
+calculateDistances(TestPoint, [Point | Rest], [Dist-Point | Distances]) :-
+  distance(Point, TestPoint, Dist),
+  calculateDistances(TestPoint, Rest, Distances).
+  
+/* Funzione che calcola la distanza euclidea tra due punti */
+distance((X1, Y1, _), (X2, Y2), Dist) :-
+  Dist is sqrt((X1 - X2) * (X1 - X2) + (Y1 - Y2) * (Y1 - Y2)).
+
+/* Ordina le distanze in ordine crescente */
+sortDistances(Distances, SortedDistances) :-
+  keysort(Distances, SortedDistances).
+
+/* Prende i primi K elementi di una lista */
+take(0, _, []).
+take(K, [X | Xs], [X | Rest]) :-
+  K > 0,
+  K1 is K - 1,
+  take(K1, Xs, Rest).
+  
+/* Rimuove il valore della distanza dal formato (Dist-(X, Y, Class)) 
+   per ottenere il formato (X, Y, Class) */
+convertToNeighbors([], []).
+convertToNeighbors([_-Neighbor | RestPairs], [Neighbor | RestNeighbors]) :-
+    convertToNeighbors(RestPairs, RestNeighbors).
+  
 /* Funzione per stampare i vicini nel formato (x, y, C) */
 printNeighbors([]).
 printNeighbors([(X, Y, C) | Rest]) :-
     format('Punto: (~2f, ~2f, ~w)~n', [X, Y, C]),
     printNeighbors(Rest).
+  
+/* Funzione che trova la classe di maggioranza tra i vicini */
+majorityClass(Neighbors, MajorityClass) :-
+    countClassOccurrences(Neighbors, CountMap),
+    maxClassOccurrences(CountMap, MajorityClass).
+
+/* Predicato ausiliario per conteggiare le occorrenze di ogni 
+  classe nei vicini */
+countClassOccurrences(Neighbors, CountMap) :-
+    countClassOccurrences(Neighbors, [], CountMap).
+
+countClassOccurrences([], CountMap, CountMap).
+countClassOccurrences([(_, _, Class) | Neighbors], PartialCountMap, CountMap) :-
+    updateCountMap(Class, PartialCountMap, NewCountMap),
+    countClassOccurrences(Neighbors, NewCountMap, CountMap).
+
+updateCountMap(Class, [], [(Class, 1)]).
+updateCountMap(Class, [(Class, Count) | Rest], [(Class, NewCount) | Rest]) :-
+    NewCount is Count + 1.
+updateCountMap(Class, [(OtherClass, Count) | Rest], [(OtherClass, Count) | NewRest]) :-
+    Class \= OtherClass,
+    updateCountMap(Class, Rest, NewRest).
+
+/* Predicato ausiliario per trovare la classe con il maggior numero di 
+  occorrenze */
+maxClassOccurrences([(Class, Count) | Rest], MajorityClass) :-
+    maxClassOccurrences(Rest, Class, Count, MajorityClass).
+
+maxClassOccurrences([], MajorityClass, _, MajorityClass).
+maxClassOccurrences([(Class, Count) | Rest], MaxClass, MaxCount, MajorityClass) :-
+    ( Count > MaxCount ->
+        maxClassOccurrences(Rest, Class, Count, MajorityClass)
+    ;   maxClassOccurrences(Rest, MaxClass, MaxCount, MajorityClass)
+    ).
 	
 % FUNZIONI AUSILIARIE	
 	
